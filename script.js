33d7209e2dc4e831ee09c1779fdad5db9c3d4b3b
EzraeZephra <CycloneAngel777@gmail.com>
2021-12-16 18:20:05 -0800

    Background Gradient introduced, added JS notes

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let js = "amazing";
 if (js === "amazing") alert("Javascript is amazing!");
 
-let number = 40 + 8 + 23 - 10;
+let number = 69; //"let" is variable declaration syntax (dynamic typing & can reassign variables later) [block scoped]
+const creationDate = 2021; //"const" variables cannot be changed - must have initial value (use these instead of let to reduce possible bugs)
+var job = "programmer"; //old way of defining variables [function-scoped]
+
 alert(number);
